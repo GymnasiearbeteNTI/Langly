be84020d20dac99b39bd419a8a43b9adbb02acba
daniel-ljung-nti-johanneberg <daniel.ljung@elev.ga.ntig.se>
2021-10-08 19:32:51 +0200

    slim saker

--- app.rb
+++ app.rb
@@ -2,17 +2,29 @@ require 'sinatra'
 require 'slim'
 
 get ('/') do
-    slim(:homepage)
+    return slim(:homepage)
 end
 
 get ('/lektioner') do
-    slim(:lessons)
+    return slim(:lessons)
 end
 
-get ('/om-oss') do
-    File.read('public/om_oss.html')
+get ('/om_oss') do
+    return slim(:om_oss)
 end
 
 get ('/login') do
-    File.read('public/login.html')
+    return slim(:login)
+end
+
+get('/lektioner/:user') do |user|
+    important_data = [user,"blomquist","gurka"]
+
+    x = {
+        name:important_data[0],
+        lname:important_data[1],
+        favfood:important_data[2]
+    }
+    "<h1>Välkommen till sidan #{x}!</h1>"
+    return slim(:lessons, locals:{key:x})
 end
