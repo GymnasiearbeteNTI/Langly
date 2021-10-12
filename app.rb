2e2f878957712d0f0aa19a1af898a5e87c581c5e
daniel-ljung-nti-johanneberg <daniel.ljung@elev.ga.ntig.se>
2021-10-12 10:37:37 +0200

    login

--- app.rb
+++ app.rb
@@ -1,8 +1,30 @@
 require 'sinatra'
 require 'slim'
+require "bcrypt"
+
+
+# hashed_password = BCrypt::Password.create "my password"
+# storable_string = hashed_password.to_s
+
+# restored_hash = BCrypt::Password.new storable_string
+
+# puts hashed_password
+
+storable_string = "$2a$12$tLL00wickBpuQQDnz381uOZz4zwrLYBm2ojHk1kneBIRvLM1auvNO"
+
+restored_hash = BCrypt::Password.new storable_string
+
+if restored_hash == "daniel29329399"
+    puts "funkar"
+end
+
+login_credentials = File.readlines("crypted_users.csv")
+p login_credentials[0]
+
 
 get ('/') do
     return slim(:homepage)
+
 end
 
 get ('/lektioner') do
@@ -21,7 +43,7 @@ get('/lektioner/:user') do |user|
     important_data = [user,"blomquist","gurka"]
 
     x = {
-        name:important_data[0],
+        fname:important_data[0],
         lname:important_data[1],
         favfood:important_data[2]
     }
