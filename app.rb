4d19dac738e1f7854b939cd4dd4009998e731246
omar-mustafa-nti-johanneberg <omar.mustafa@elev.ga.ntig.se>
2021-11-16 10:43:06 +0100

    update

--- app.rb
+++ app.rb
@@ -85,7 +85,8 @@ end
 
 get('/teori_spanska/visual') do
     visual_learning = {
-        title:"Visual Learning"
+        title:"Visual Learning",
+        intro:"Here you will learn with a little help from images and other visual media #{session[:loggedin_user]}!"
     }
     return slim(:teori_spanska, locals:{key:visual_learning})
 end
