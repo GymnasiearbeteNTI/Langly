4f2dde00192a8774f5125c12dcc2c85667fb6e4c
omar-mustafa-nti-johanneberg <omar.mustafa@elev.ga.ntig.se>
2022-01-24 19:59:05 +0100

    quiz done

--- db_saker.rb
+++ db_saker.rb
@@ -8,83 +8,20 @@ enable :sessions
 db = SQLite3::Database.new('db/db.db')
 db.results_as_hash = true
 
-# Creates a user by taking username and password from the form, encrypts password and inserts them into the users table
-def create()
-    db = SQLite3::Database.new('db/db.db')
-    db.results_as_hash = true
-    name = params["name"]
-    password = BCrypt::Password.create(params["pass"])
-    db.execute('INSERT INTO users(Username, Password) VALUES(?, ?)', name, password) 
-    redirect('/')
-end
-
-# Checks if there is a username entered, if the username has a match in the database and if the correct password was entered
-def login()
-    db = SQLite3::Database.new('db/db.db')
-    result = db.execute("SELECT Password FROM users WHERE Username =(?)", params["name"])
-    if result[0] == nil
-        session[:loginfail] = true
-        redirect('/')
-    end
-    not_password = result[0][0]
-    if BCrypt::Password.new(not_password) == params["pass"]
-        session[:loggedin] = true
-        session[:user] = params["name"]
-        redirect('/')
-    else
-        session[:loginfail] = true
-        redirect('/')
-    end
-end
-
-# Creates a thread by taking title, description from the form, userid from the session and users table and inserts that into the threads table
-def new_thread()
-    db = SQLite3::Database.new('db/db.db')
-    opid = db.execute("SELECT UserId FROM users WHERE Username=(?)", session[:user])
-    if opid != nil
-        db.execute("INSERT INTO threads(Title, Description, OpId) VALUES(?, ?, ?)", params["title"], params["description"], opid)
-    else
-        redirect('/')
-    end
-    redirect('/')
-end
-
-# Takes the text for threads and replies from the database and puts them into the locals dictionary so they can be displayed on the site
-def index()
-    db = SQLite3::Database.new('db/db.db')
-    db.results_as_hash = true
-    threads = db.execute("SELECT threads.*, users.Username FROM threads INNER JOIN users ON threads.OpId = users.UserId")
-    replies = db.execute("SELECT replies.*, users.Username FROM replies INNER JOIN users ON replies.AuthorId = users.UserId")
-    slim(:index, locals:{threads: threads, replies: replies})
-end
-
-# Takes text from the form and inserts into the replies table. Also takes id of the thread it was posted in and id of the user who posted the reply
-def reply()
-    db = SQLite3::Database.new('db/db.db')
-    authorid = db.execute("SELECT UserId FROM users WHERE Username=(?)", session[:user])
-    if authorid != nil
-        db.execute("INSERT INTO replies(Text, AuthorId, ParentId) VALUES(?, ?, ?)", params["reply"], authorid, params["threadId"])
-    else
-        redirect('/')
-    end
-    redirect('/')
-end
-
-def delete_thread()
-    db = SQLite3::Database.new('db/db.db')
-end
-
 def registerfunc()
     db = SQLite3::Database.new('db/db.db')
     db.results_as_hash = true
     uname = params["reg_username"]
     pword = BCrypt::Password.create(params["reg_password"])
+
     begin
         db.execute('INSERT INTO users(Username, Password) VALUES(?, ?)', uname, pword)
     rescue
         session[:reg_error] = true
         redirect('/register')
     end
+
+    session[:account_created] = "Account created successfully!"
     redirect('/login')
 end
 
